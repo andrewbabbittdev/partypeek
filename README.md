4b54f574eb08b49688521092831229f6e8b3b08a
Andrew Babbitt <andrew.babbitt@outlook.com>
2025-12-26 18:37:08 -0700

    chore: initial source

---
+++ README.md
@@ -0,0 +1,19 @@
+# PartyPeek
+[![Build](https://github.com/andrewbabbittdev/partypeek/actions/workflows/build-code.yml/badge.svg?branch=main&event=push)](https://github.com/andrewbabbittdev/partypeek/actions/workflows/build-code.yml)
+[![Lint](https://github.com/andrewbabbittdev/partypeek/actions/workflows/lint-code.yml/badge.svg?branch=main&event=push)](https://github.com/andrewbabbittdev/partypeek/actions/workflows/lint-code.yml)
+
+## What is Party Peek?
+Party Peek is a plugin for peeking at who's in a party before joining.
+
+## What is in this repo?
+This repository contains all source code for Party Peek. Everything needed to develop or deploy Party Peek is self contained in this repository.
+
+## Getting started
+To start developing Party Peek you need just a few things:
+
+- [.NET 10 SDK](https://dotnet.microsoft.com/en-us/download/dotnet/10.0)
+- [Final Fantasy XIV](https://na.finalfantasyxiv.com/)
+- [Dalamud](https://dalamud.dev/faq/getting-started/)
+
+## Run PartyPeek
+Once you have all the dependencies listed above load the plugin in Dalamud and the plugin will automatically show party members when opening a party finder listing.
---
+++ src/Plugin.cs
@@ -0,0 +1,42 @@
+﻿// Licensed to the Party Peek Contributors under one or more agreements.
+// The Party Peek Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Dalamud.Hosting;
+using Dalamud.Plugin;
+
+namespace PartyPeek;
+
+/// <summary>
+/// Represents a Dalamud plugin that manages its application lifecycle and integrates with the Dalamud plugin
+/// environment.
+/// </summary>
+public class Plugin : IDalamudPlugin
+{
+    /// <summary>
+    /// Gets or sets the current instance of the Dalamud application used by the plugin framework.
+    /// </summary>
+    public static DalamudApplication App { get; private set; } = default!;
+
+    /// <summary>
+    /// Initializes a new instance of the Plugin class using the specified Dalamud plugin interface.
+    /// </summary>
+    /// <param name="pluginInterface">The interface provided by Dalamud for interacting with the plugin environment. Cannot be null.</param>
+    public Plugin(IDalamudPluginInterface pluginInterface)
+    {
+        var builder = DalamudApplication.CreateBuilder(pluginInterface);
+
+        App = builder.Build();
+
+        App.Start();
+    }
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        App.Stop();
+        App.Dispose();
+
+        GC.SuppressFinalize(this);
+    }
+}
